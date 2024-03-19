20c2f72c891077db4d3d5cb7e3e166e18135ef13
alexcrawford020@gmail.com <59071533+Alex0247@users.noreply.github.com>
2024-03-19 04:11:40 +0000

    Added Login Authentication

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from cryptography.fernet import Fernet
 
 def obtainUser():
-    userName = input("Enter Username: ")
+    userName = input("\nEnter Username: ")
     return userName
 
 def obtainPassword():
@@ -14,13 +14,11 @@ def encryptPassword(password):
     key = Fernet.generate_key()
     f = Fernet(key)
     token = f.encrypt(password.encode())
-    print(f"token: {token}")
     return token, f
 
 # Decrypts Password
 def decryptPassword(f, password):
-    return f.decrypt(password) 
-
+    return f.decrypt(password)
 
 def addUser():
     username = obtainUser()
@@ -28,12 +26,34 @@ def addUser():
     token, f = encryptPassword(password)
     return {"username": username, "token": token, "key": f}
 
+
+def loginUser(user_data):
+    inputUser = input("\nPlease Enter Your Username To A Valid Account: ")
+    inputPassword = input("Please Enter Your Passphrase To A Valid Account: ")
+
+    if inputUser != user_data['username'] or inputPassword != decryptPassword(user_data['key'], user_data['token']).decode():
+        print("\n-----------------")
+        print("-Login Incorrect-")
+        print("-----------------")
+
+        return False
+    else:
+        print("\n------------------")
+        print("-Login Successful-")
+        print("------------------")
+        return True
+
 def main():
-    # for every item in account output result
-    user_data = addUser()
-    print(user_data)
-    print(f"\nUsername: {user_data['username']}")
-    print(f"Password: {decryptPassword(user_data['key'], user_data['token'])}")
+    while(True):
+        flag = False
+        userInput = input("\nPress '1' to add an account\nPress '2' to login\nEnter Here: ")
+        if userInput == '1':
+            user_data = addUser()
+            print(user_data)
+        else:
+            flag = loginUser(user_data)
+            if flag:
+                break
 
 if __name__ == "__main__":
     main()
