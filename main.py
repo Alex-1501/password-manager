0c2df137a62831f26554c56f2949c1fb38ba2f52
alexcrawford020@gmail.com <59071533+Alex0247@users.noreply.github.com>
2024-03-19 03:13:14 +0000

    Renamed 't' to 'token' for clarity

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import bcrypt
 from cryptography.fernet import Fernet
 
 def obtainUser():
@@ -9,28 +8,31 @@ def obtainPassword():
     password = input("Enter Password: ")
     return password
 
+# Encrypts Password
 def encryptPassword(password):
+    # Generates A Key
     key = Fernet.generate_key()
+    # Creates Fernet Token
     f = Fernet(key)
-    t = f.encrypt(password.encode())
-    return t, f
+    token = f.encrypt(password.encode())
+    return token, f
 
-def decryptPassword(key, password):
-    return key.decrypt(password)
+def decryptPassword(token, password):
+    return token.decrypt(password)
 
 
 def addUser():
     username = obtainUser()
     password = obtainPassword()
-    encryptedPassword, key = encryptPassword(password)
-    return {"username": username, "password": encryptedPassword, "key": key}
+    encryptedPassword, token = encryptPassword(password)
+    return {"username": username, "password": encryptedPassword, "token": token}
 
 def main():
     # for every item in account output result
     user_data = addUser()
     print(user_data)
     print(f"\nUsername: {user_data['username']}")
-    print(f"Password: {decryptPassword(user_data['key'], user_data['password'])}")
+    print(f"Password: {decryptPassword(user_data['token'], user_data['password'])}")
 
 if __name__ == "__main__":
     main()
